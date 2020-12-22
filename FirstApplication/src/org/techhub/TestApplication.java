1a41360346eea100e17441da84b6ce25ad1b289c
Danny12-beep <75467979+Danny12-beep@users.noreply.github.com>
2020-12-22 21:26:03 +0530

    Initial commit

--- FirstApplication/src/org/techhub/TestApplication.java
+++ FirstApplication/src/org/techhub/TestApplication.java
@@ -5,7 +5,7 @@ public class TestApplication {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("good morning");
-
+		System.out.println("good afternoon"); 
 	}
 
 }
